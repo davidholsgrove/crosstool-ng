f86f9b450209a2c4cd0c70deace29910ddd1e731
David Holsgrove <david.holsgrove@xilinx.com>
2013-03-12 15:16:10 +1000

    cc/gcc: Add debug symbols option
    Signed-off-by: David Holsgrove <david.holsgrove@xilinx.com>

--- scripts/build/cc/gcc.sh
+++ scripts/build/cc/gcc.sh
@@ -379,6 +379,12 @@ do_cc_core_backend() {
         extra_config+=("--disable-multilib")
     fi
 
+    if [ "${CT_CC_DEBUG_SYMBOLS}" = "y" ]; then
+        CT_DoLog DEBUG "Building gcc with debug symbols"
+        cflags+=" -g -O0"
+        extra_config+=("--enable-checking")
+    fi
+
     CT_DoLog DEBUG "Extra config passed: '${extra_config[*]}'"
 
     # Use --with-local-prefix so older gccs don't look in /usr/local (http://gcc.gnu.org/PR10532)
@@ -840,6 +846,12 @@ do_cc_backend() {
     # Since that's the default, only pass --disable-multilib.
     if [ "${CT_MULTILIB}" != "y" ]; then
         extra_config+=("--disable-multilib")
+    fi
+
+    if [ "${CT_CC_DEBUG_SYMBOLS}" = "y" ]; then
+        CT_DoLog DEBUG "Building gcc with debug symbols"
+        cflags+=" -g -O0"
+        extra_config+=("--enable-checking")
     fi
 
     CT_DoLog DEBUG "Extra config passed: '${extra_config[*]}'"
